f8b53a4fc98ebee0c99900dd30e9ab60057ff454
Matt Hunt <mattlethargic@gmail.com>
2021-10-19 21:07:06 +0100

    commenting out fd

--- terraform/front-door.tf
+++ terraform/front-door.tf
@@ -1,44 +1,44 @@
-resource "azurerm_frontdoor" "app" {
-  name                                         = format("propt-app-%s-ukso-fd", var.environment)
-  resource_group_name                          = azurerm_resource_group.app.name
-  enforce_backend_pools_certificate_name_check = false
+# resource "azurerm_frontdoor" "app" {
+#   name                                         = format("propt-app-%s-ukso-fd", var.environment)
+#   resource_group_name                          = azurerm_resource_group.app.name
+#   enforce_backend_pools_certificate_name_check = false
 
-  tags = local.common_tags
+#   tags = local.common_tags
 
-  routing_rule {
-    name               = "exampleRoutingRule1"
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    frontend_endpoints = ["exampleFrontendEndpoint1"]
-    forwarding_configuration {
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "exampleBackendBing"
-    }
-  }
+#   routing_rule {
+#     name               = "exampleRoutingRule1"
+#     accepted_protocols = ["Http", "Https"]
+#     patterns_to_match  = ["/*"]
+#     frontend_endpoints = ["propt-app"]
+#     forwarding_configuration {
+#       forwarding_protocol = "MatchRequest"
+#       backend_pool_name   = "propt-app-backend"
+#     }
+#   }
 
-  backend_pool_load_balancing {
-    name = "exampleLoadBalancingSettings1"
-  }
+#   backend_pool_load_balancing {
+#     name = "propt-app-load-balancing"
+#   }
 
-  backend_pool_health_probe {
-    name = "exampleHealthProbeSetting1"
-  }
+#   backend_pool_health_probe {
+#     name = "propt-app-health-probe"
+#   }
 
-  backend_pool {
-    name = "exampleBackendBing"
-    backend {
-      host_header = "www.bing.com"
-      address     = "www.bing.com"
-      http_port   = 80
-      https_port  = 443
-    }
+#   backend_pool {
+#     name = "propt-app-backend"
+#     backend {
+#       host_header = "www.bing.com"
+#       address     = "www.bing.com"
+#       http_port   = 80
+#       https_port  = 443
+#     }
 
-    load_balancing_name = "exampleLoadBalancingSettings1"
-    health_probe_name   = "exampleHealthProbeSetting1"
-  }
+#     load_balancing_name = "propt-app-load-balancing"
+#     health_probe_name   = "propt-app-health-probe"
+#   }
 
-  frontend_endpoint {
-    name      = "exampleFrontendEndpoint1"
-    host_name = "example-FrontDoor.azurefd.net"
-  }
-}
+#   frontend_endpoint {
+#     name      = "propt-app"
+#     host_name = "propt-app-test.azurefd.net"
+#   }
+# }
